22230ad5058bcdfa7d22463a6888ad75e0d7a9bc
Xavier Hua <e1186218@u.nus.edu>
2025-05-15 23:05:22 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: '/HuaAutoElectrical/'  // Replace <REPO_NAME> with your repo name
 })
