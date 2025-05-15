8d0275811543e66314560e367062b03e3213b361
Xavier Hua <e1186218@u.nus.edu>
2025-05-15 22:23:08 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/hua-auto-electrical/',  // <-- IMPORTANT: Use your repo name
+  base: '/HuaAutoElectrical/',  // <-- IMPORTANT: Use your repo name
   plugins: [vue()]
 })
